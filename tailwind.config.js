ec1e5a4ed82ba8ab33d4d88193e59f5fa865e1c1
MarieTurco <marieturco.07@gmail.com>
2025-12-11 00:09:49 +0100

    retouche couleurs

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,12 @@ tailwind.config = {
     theme: {
         extend: {
             colors: {
-                'light-50': '#facce0',
+                'light-50': '#F3A895',
                 'light-100': '#FCE2ED',
-                'light-200': '#F48CB840',
-                'light-300': '#E8A9C3',
-                'dark-100': '#A10550',
-                'dark-200': '#b6276a',
+                'light-200': '#F7EBDF',
+                'light-300': '#F7C2B5',
+                'dark-100': '#51161C',
+                'dark-200': '#772C34',
                 'dark-300': '#7e043e',
                 'gray-100': '#555555',
                 'gray-200': '#040404'
